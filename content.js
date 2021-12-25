736ceddf0f3f4d11d8b94e0cb430e1c5136b4533
Adstefnum <bauerstevenson@gmail.com>
2021-12-25 16:05:00 +0200

    all ratings and points gain or loss are now captured by regex

--- content.js
+++ content.js
@@ -1,22 +1,16 @@
 var elements = document.getElementsByTagName('*');
 
-/*<div class="upt__info__ratings"><span title="Rapid rating over 244 games" data-icon="" 
-class="text">1442</span><span title="Classical rating over 17 games" data-icon="" 
-class="text">1310?</span><span title="Blitz rating over 31 games" data-icon="" class="text">1237
-</span><span title="Bullet rating over 3 games" data-icon="" class="text">909?</span><span 
-title="UltraBullet rating over 0 games" data-icon="" class="text">&nbsp;&nbsp;&nbsp;-</span><span
- title="Crazyhouse rating over 0 games" data-icon="" class="text">&nbsp;&nbsp;&nbsp;-</span><span 
- title="Chess960 rating over 0 games" data-icon="" class="text">&nbsp;&nbsp;&nbsp;-</span><span 
- title="King of the Hill rating over 0 games" data-icon="" class="text">&nbsp;&nbsp;&nbsp;-
- </span></div>*/
+// var patterns = [
+//     "\\d{4}\\?",//2004?
+//     "\\d{4}",//
+//     "\\d{3}\\?",//200?
+//     "\\d{3}",//200
+//     "\\+\\d+",//+10 +6
+//     "\\−\\d+",//-10 -6
+// ]
 
-//<a class="user-link" href="/@/ichbinkrass123">ichbinkrass123 (1480) <good>+6</good></a>
+// var pattern = `/(${patterns.join("|")})/g`;
 
-//<rating>1441</rating>
-
-//<rating><strong>909?</strong>  <span>3 games</span></rating>
-
-//<a class="user-link ulpt" href="/@/Ad_stefnum">Ad_stefnum</a>
 
 
 for (var i = 0; i < elements.length; i++) {
@@ -28,10 +22,9 @@ for (var i = 0; i < elements.length; i++) {
         if (node.nodeType === 3) {
             var text = node.nodeValue;
             
-            ///(\d{3}\?)$/g|/(\d{4}\?)$/g|/(\d{4})$/g|/(\d{3})$/g;
-            ///(\d{4}|\d{4}\-\d{4})$/g
-                var pattern = /(\d{4}\?|\d{3}|\d{3}\?|\d{4})$/g;
-            
+            //2004?|2000|200?|200|-6 or -10|+6 or +10|
+                var pattern = /(\d{4}\?|\d{4}|\d{3}\?|\d{3}|\−\d+|\+\d+)/g;
+            console.log(pattern);
             var replacedText = text.replaceAll(pattern, "");
 
             if (replacedText !== text) {
